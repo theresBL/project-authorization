c41be52cf4eccbd83ffa721b169c4be173e7dd21
Vio Szabo <vio@vioszabo.se>
2023-05-29 14:02:27 +0200

    fix sidescroll issue

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -8,30 +8,30 @@ import headerImg from '../images/Background.svg'
 import { StartButton } from 'lib/StartButton'
 
 export const Login = () => {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [mode, setMode] = useState("login")
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [mode, setMode] = useState('login')
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const accessToken = useSelector(store => store.user.accessToken);
+  const accessToken = useSelector((store) => store.user.accessToken)
   useEffect(() => {
     if (accessToken) {
-      navigate("/")
+      navigate('/')
     }
   }, [accessToken])
 
   const onFormSubmit = (event) => {
     event.preventDefault()
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({ username: username, password: password })
     }
     fetch(API_URL(mode), options)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         if (data.success) {
           dispatch(user.actions.setAccessToken(data.response.accessToken))
           dispatch(user.actions.setUsername(data.response.username))
@@ -48,157 +48,159 @@ export const Login = () => {
   return (
     <Main>
       <LoginPageTop>
-      <HeaderText>Circ(le) it!</HeaderText>
-      <HeaderImg src={headerImg} alt="headerImg" />
+        <HeaderText>Circ(le) it!</HeaderText>
+        <HeaderImg src={headerImg} alt="headerImg" />
       </LoginPageTop>
       <LoginPageBottom>
-      <StartDiv>
-      <StartText>Get going right away!</StartText>
-      <StartButton type="submit">Start a workout</StartButton>
-      </StartDiv>
-      <LoginText>Want to be able to save workouts? Sign up here.</LoginText>
-      <RadioDiv>
-        <RadioDivSmall>
-          <label htmlFor="signup">Sign up</label>
-          <input
-            type="radio"
-            id="signup"
-            checked={mode === "signup"}
-            onChange={() => setMode("signup")} />
-        </RadioDivSmall>
-        <RadioDivSmall>
-          <label htmlFor="login">Log in</label>
-          <input
-            type="radio"
-            id="login"
-            checked={mode === "login"}
-            onChange={() => setMode("login")} />
-        </RadioDivSmall>
-      </RadioDiv>
-      <SubmitForm onSubmit={onFormSubmit}>
-        <FormDiv>
-          <label htmlFor="username" />
-          <input
-            type="text"
-            id="username"
-            placeholder="Username"
-            value={username}
-            onChange={e => setUsername(e.target.value)} />
-          <label htmlFor="password" />
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            value={password}
-            onChange={e => setPassword(e.target.value)} />
-        </FormDiv>
-        <StartButton white type="submit">Log in</StartButton>
-      </SubmitForm>
+        <StartDiv>
+          <StartText>Get going right away!</StartText>
+          <StartButton type="submit">Start a workout</StartButton>
+        </StartDiv>
+        <LoginText>Want to be able to save workouts? Sign up here.</LoginText>
+        <RadioDiv>
+          <RadioDivSmall>
+            <label htmlFor="signup">Sign up</label>
+            <input
+              type="radio"
+              id="signup"
+              checked={mode === 'signup'}
+              onChange={() => setMode('signup')}
+            />
+          </RadioDivSmall>
+          <RadioDivSmall>
+            <label htmlFor="login">Log in</label>
+            <input
+              type="radio"
+              id="login"
+              checked={mode === 'login'}
+              onChange={() => setMode('login')}
+            />
+          </RadioDivSmall>
+        </RadioDiv>
+        <SubmitForm onSubmit={onFormSubmit}>
+          <FormDiv>
+            <label htmlFor="username" />
+            <input
+              type="text"
+              id="username"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password" />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormDiv>
+          <StartButton white type="submit">
+            Log in
+          </StartButton>
+        </SubmitForm>
       </LoginPageBottom>
     </Main>
   )
 }
 
 const Main = styled.div`
-width: 100%; 
-height: 100vh;
-gap: 16px;
-display: flex;
-flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  gap: 16px;
+  display: flex;
+  flex-direction: column;
 
-@media (min-width:1024px) {
-  max-width: 100vw;
-  max-height: 100vh;
-}
+  @media (min-width: 1024px) {
+    max-width: 100vw;
+    max-height: 100vh;
+  }
 `
 const LoginPageTop = styled.div`
-
-  @media (min-width:1024px) {
-    flex-basis:20%;
+  @media (min-width: 1024px) {
+    flex-basis: 20%;
   }
 `
 const HeaderImg = styled.img`
-display: flex;
-width: 100vw;
-object-fit: cover;
-object-position: center;
+  display: flex;
+  width: 100vw;
+  object-fit: cover;
+  object-position: center;
 
-@media (min-width:1024px) {
-  height: 30vh;
-}
+  @media (min-width: 1024px) {
+    height: 30vh;
+  }
 `
 
 const HeaderText = styled.h1`
-color: white;
-font-size: 45px;
-position: absolute;
-top: calc(10%);
-left: calc(10%);
-
-
-@media (min-width: 1024px){
-  font-size: 82px;
+  color: white;
+  font-size: 45px;
+  position: absolute;
   top: calc(10%);
-}
+  left: calc(10%);
+
+  @media (min-width: 1024px) {
+    font-size: 82px;
+    top: calc(10%);
+  }
 `
 const LoginPageBottom = styled.div`
-width: 390px;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-margin: 0;
-align-self: center;
+  width: 375px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  align-self: center;
 
-@media (min-width:1024px) {
-justify-content: center;
-}
+  @media (min-width: 1024px) {
+    justify-content: center;
+  }
 `
 
 const StartDiv = styled.div`
-display: flex;
-flex-direction: column;
-gap: 2px;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  align-items: center;
 `
 
 const StartText = styled.p`
-font-size: 24px;
+  font-size: 24px;
 `
 
 const LoginText = styled.p`
-font-size: 12px;
-margin-top: 3em;
-margin-bottom: 0;
+  font-size: 12px;
+  margin-top: 3em;
+  margin-bottom: 0;
 `
 
 const RadioDiv = styled.div`
-display: flex;
-gap: 16px;
+  display: flex;
+  gap: 16px;
 `
 
 const RadioDivSmall = styled.div`
-align-items:center;
+  align-items: center;
 `
 
 const FormDiv = styled.div`
-flex-direction:column;
-display: flex;
-gap: 10px;
-
+  flex-direction: column;
+  display: flex;
+  gap: 10px;
 `
 
 const SubmitForm = styled.form`
-display: flex;
-flex-direction: column;
-gap: 10px;
-width: 70%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 70%;
 
-input{
-  border-radius: 10px;
-  padding: 10px;
-  border:none; 
-  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.5);
-
-}
+  input {
+    border-radius: 10px;
+    padding: 10px;
+    border: none;
+    box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.5);
+  }
 `
